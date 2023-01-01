c5ec8563375daeadb73891dde5151f879eac8424
konrad <konrad1984@icloud.com>
2023-01-01 11:31:30 +0000

    eggdrop 1.6 tcl

--- kick.tcl
+++ kick.tcl
@@ -1,4 +1,4 @@
-# Zdefiniuj last_nick jako $botnick - jesli nie jest jeszcze zdefiniowany:
+## Zdefiniuj last_nick jako $botnick - jesli nie jest jeszcze zdefiniowany:
 
 if {![info exists last_nick]} {set last_nick $botnick}
 
@@ -6,24 +6,24 @@ proc wypierdol_kogos {nick uhost handle chan args} {
   global botnick last_nick
   global rep
 
-# Zdefiniuj last_nick jako $botnick jesli last_nick opuscil kanal:
+## Zdefiniuj last_nick jako $botnick jesli last_nick opuscil kanal:
 
   if {![onchan $last_nick $chan]} {set last_nick $botnick}
   set mylist [chanlist $chan]
 
-# Usun bota z listy nickow do wypierdolenia:
+## Usun bota z listy nickow do wypierdolenia:
 
   set wb [lsearch $mylist $botnick]
   set mylist [lreplace $mylist $wb $wb]
 
-# Jesli last_nick jest botem - OK. Jesli nie jest, usun ten nick z listy do wypierdolenia:
+## Jesli last_nick jest botem - OK. Jesli nie jest, usun ten nick z listy do wypierdolenia:
 
   if {$last_nick != $botnick} {
     set wl [lsearch $mylist $last_nick]
     set mylist [lreplace $mylist $wl $wl]
   }
 
-# Wybierz losowy nick na kanale. Upewnij sie, ze nie jestes sam na kanale:
+## Wybierz losowy nick na kanale. Upewnij sie, ze nie jestes sam na kanale:
 
   set mylength [llength $mylist]
   if {$mylength == 0} {
@@ -33,19 +33,19 @@ proc wypierdol_kogos {nick uhost handle chan args} {
   set myindex [rand $mylength]
   set result [lindex $mylist $myindex]
 
-# Zbuduj napiecie za pomoca przeklenstw i wypierdol kogos:
+## Zbuduj napiecie za pomoca przeklenstw i wypierdol kogos:
 
   putserv "PRIVMSG $chan :Spoko!"
   putserv "NOTICE $chan :Uwaga frajerzy, zaraz kogos wypierdole!"
   putserv "PRIVMSG $chan :Ej, $result, wypierdalasz!"
   putserv "KICK $chan $result :Chuj ci w dupe nygusie!"
 
-# Zdefiniuj last_nick do kolejnej rundy:
+## Zdefiniuj last_nick do kolejnej rundy:
 
   set last_nick $result
   return 1
 }
 
-# Publiczna komenda do wypierdalania:
+## Publiczna komenda do wypierdalania:
 
 bind pubm - "*!wypierdol*" wypierdol_kogos
